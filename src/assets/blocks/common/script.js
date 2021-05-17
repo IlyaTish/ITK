e882f6f9452e93679159bff818c1198997339044
Илья Тищенко <ilya.tishencko676@yandex.ru>
2021-05-17 13:05:40 +0300

    Navigation, filters

--- src/assets/blocks/common/script.js
+++ src/assets/blocks/common/script.js
@@ -13,6 +13,7 @@ const ready = callback => {
   else document.addEventListener('DOMContentLoaded', callback)
 }
 
+
 const initSwiper = () => {
   const tabsSwiper = new Swiper('.tabs-swiper', {
     slidesPerView: 'auto',
@@ -29,7 +30,66 @@ const appendElem = (elem, cont) => {
 }
 
 
+const initSelect = () => {
+  const selectBox = document.querySelectorAll('.select-box');
+
+  document.addEventListener('click', e => {
+    let reversedIdx = [];
+
+    [].forEach.call(selectBox, (select, selectIndex) => {
+      const selectBoxCont = select.querySelector('.select-box__cont'),
+            dropDown      = select.querySelector('.select-box__dropdown'),
+            dropDownItems = dropDown.querySelectorAll('.select-box__dropdown li'),
+
+            activeClass   = 'active',
+            isClickInside = select.contains(e.target);
+
+      reversedIdx.push(selectIndex);
+
+      [].forEach.call(dropDownItems, item => {
+        const selectedOption = item.textContent;
+
+        item.addEventListener('click', e => {
+          [].forEach.call(dropDownItems, item => item.classList.remove('active'));
+          e.currentTarget.classList.add('active');
+
+          if (select.classList.contains('selectable'))
+            selectBoxCont.innerHTML = selectedOption;
+        })
+      });
+
+      if (select !== e.target && !select.contains(e.target)) {
+        if (dropDown !== e.target) {
+          select.classList.remove('active');
+
+          if (dropDown !== null)
+            dropDown.style.display = 'none'
+        }
+      }
+
+      else {
+        if (dropDown !== e.target) {
+          select.classList.toggle('active');
+
+          if (dropDown !== null) {
+            dropDown.style.display =
+              (dropDown.style.display == 'none') ? 'block' : 'none'
+          }
+        }
+      }
+    });
+
+    reversedIdx.reverse();
+
+    reversedIdx.map((idxItem, index) =>
+      selectBox[index].style.zIndex = `999${idxItem}`
+    )
+  })
+}
+
+
 
 // -------- Functions execution --------
 
+initSelect();
 initSwiper();
